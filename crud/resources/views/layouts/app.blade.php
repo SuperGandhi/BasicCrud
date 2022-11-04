6d1d291ceeb302c668d37fdd963e42ba606efa93
SuperGandhi <patrickborjabueno@hotmail.com>
2022-11-04 00:50:52 -0500

    Add features to table

--- crud/resources/views/layouts/app.blade.php
+++ crud/resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" 
     href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.1/tailwind.min.css">
     <link href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/a23e6feb03.js"></script>
   </head>
   <body class="bg-gray-100 text-gray-800">
         <main class="p-16 flex justify-center">
